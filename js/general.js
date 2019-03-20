fe572a854fcdc1420ef82204e3176bcb71367e9c
Liangzheng Luo <hermitluo@gmail.com>
2019-03-21 00:53:14 +0800

    3.21 Cursor Update
    Customized cursor now only works at index.html.

--- js/general.js
+++ js/general.js
@@ -1,5 +1,10 @@
 var cursor = document.getElementById("cursor"),
     body = document.body;
+var path = window.location.pathname;
+var page = path.split("/").pop();
+var thre = body.clientHeight;
+let colorShironeri = "rgb(252, 250, 242)",
+    colorKon = "rgb(17,35,64)";
 
 function MoveCursor() {
     var Y = event.pageY,
@@ -10,3 +15,10 @@ function MoveCursor() {
 }
 
 window.onmousemove = MoveCursor;
+window.onmouseover = function (e) {
+    if (e.target.className == 'footer-nav-content') {
+        cursor.style.background = colorShironeri;
+    } else {
+        cursor.style.background = colorKon;
+    }
+}
