e0353d857f4677607e5b6bbce4ddba5811c3bde8
RakuKun <liangzheng0730@gmail.com>
2020-01-30 00:58:36 -0800

    1.30 Major Update
    Updated hyperlink and transition, added some content, universalized css layout, added scrollbar active effect. I change so many files so it is definitely a major update.

--- js/general2.0.js
+++ js/general2.0.js
@@ -6,134 +6,136 @@ var cursor = {
   endY: window.innerHeight / 2,
   cursorVisible: true,
   cursorEnlarged: false,
-  $dot: document.querySelector('#cursor'),
-  $outline: document.querySelector('#cursor-outline'),
+  $dot: document.querySelector("#cursor"),
+  $outline: document.querySelector("#cursor-outline"),
 
   init: function() {
-    this.dotSize = this.$dot.offsetWidth
-    this.outlineSize = this.$outline.offsetWidth
+    this.dotSize = this.$dot.offsetWidth;
+    this.outlineSize = this.$outline.offsetWidth;
 
-    this.setupEventListeners()
-    this.animateDotOutline()
+    this.setupEventListeners();
+    this.animateDotOutline();
   },
 
   setupEventListeners: function() {
-    var self = this
+    var self = this;
 
     function registerMouseEvent(el) {
-      el.addEventListener('mouseover', function() {
-        self.cursorEnlarged = true
-        self.toggleCursorSize()
-      })
-      el.addEventListener('mouseout', function() {
-        self.cursorEnlarged = false
-        self.toggleCursorSize()
-      })
+      el.addEventListener("mouseover", function() {
+        self.cursorEnlarged = true;
+        self.toggleCursorSize();
+      });
+      el.addEventListener("mouseout", function() {
+        self.cursorEnlarged = false;
+        self.toggleCursorSize();
+      });
     }
     // Anchor hovering
-    document.querySelectorAll('a').forEach(function(el) {
-      registerMouseEvent(el)
-    })
+    document.querySelectorAll("a").forEach(function(el) {
+      registerMouseEvent(el);
+    });
 
-    document.querySelectorAll('.top').forEach(function(el) {
-      registerMouseEvent(el)
-    })
+    document.querySelectorAll(".top").forEach(function(el) {
+      registerMouseEvent(el);
+    });
 
     // Click events
-    document.addEventListener('mousedown', function() {
-      self.cursorEnlarged = true
-      self.toggleCursorSize()
-    })
-    document.addEventListener('mouseup', function() {
-      self.cursorEnlarged = false
-      self.toggleCursorSize()
-    })
+    document.addEventListener("mousedown", function() {
+      self.cursorEnlarged = true;
+      self.toggleCursorSize();
+    });
+    document.addEventListener("mouseup", function() {
+      self.cursorEnlarged = false;
+      self.toggleCursorSize();
+    });
 
-    document.addEventListener('mousemove', function(e) {
+    document.addEventListener("mousemove", function(e) {
       // Show the cursor
-      self.cursorVisible = true
-      self.toggleCursorVisibility()
+      self.cursorVisible = true;
+      self.toggleCursorVisibility();
 
       // Position the dot
-      self.endX = e.pageX
-      self.endY = e.pageY
-      self.$dot.style.top = self.endY + 'px'
-      self.$dot.style.left = self.endX + 'px'
-    })
+      self.endX = e.pageX;
+      self.endY = e.pageY;
+      self.$dot.style.top = self.endY + "px";
+      self.$dot.style.left = self.endX + "px";
+    });
 
     // Hide/show cursor
-    document.addEventListener('mouseenter', function(e) {
-      self.cursorVisible = true
-      self.toggleCursorVisibility()
-      self.$dot.style.opacity = 1
-      self.$outline.style.opacity = 1
-    })
+    document.addEventListener("mouseenter", function(e) {
+      self.cursorVisible = true;
+      self.toggleCursorVisibility();
+      self.$dot.style.opacity = 1;
+      self.$outline.style.opacity = 1;
+    });
 
-    document.addEventListener('mouseleave', function(e) {
-      self.cursorVisible = true
-      self.toggleCursorVisibility()
-      self.$dot.style.opacity = 0
-      self.$outline.style.opacity = 0
-    })
+    document.addEventListener("mouseleave", function(e) {
+      self.cursorVisible = true;
+      self.toggleCursorVisibility();
+      self.$dot.style.opacity = 0;
+      self.$outline.style.opacity = 0;
+    });
   },
 
   animateDotOutline: function() {
-    var self = this
+    var self = this;
 
-    self._x += (self.endX - self._x) / self.delay
-    self._y += (self.endY - self._y) / self.delay
-    self.$outline.style.top = self._y + 'px'
-    self.$outline.style.left = self._x + 'px'
+    self._x += (self.endX - self._x) / self.delay;
+    self._y += (self.endY - self._y) / self.delay;
+    self.$outline.style.top = self._y + "px";
+    self.$outline.style.left = self._x + "px";
 
-    requestAnimationFrame(this.animateDotOutline.bind(self))
+    requestAnimationFrame(this.animateDotOutline.bind(self));
   },
 
   toggleCursorSize: function() {
-    var self = this
+    var self = this;
 
     if (self.cursorEnlarged) {
-      self.$dot.style.transform = 'translate(-50%, -50%) scale(0.75)'
-      self.$outline.style.transform = 'translate(-50%, -50%) scale(1.5)'
+      self.$dot.style.transform = "translate(-50%, -50%) scale(0.75)";
+      self.$outline.style.transform = "translate(-50%, -50%) scale(1.5)";
     } else {
-      self.$dot.style.transform = 'translate(-50%, -50%) scale(1)'
-      self.$outline.style.transform = 'translate(-50%, -50%) scale(1)'
+      self.$dot.style.transform = "translate(-50%, -50%) scale(1)";
+      self.$outline.style.transform = "translate(-50%, -50%) scale(1)";
     }
   },
 
   toggleCursorVisibility: function() {
-    var self = this
+    var self = this;
 
     if (self.cursorVisible) {
-      self.$dot.style.opacity = 1
-      self.$outline.style.opacity = 1
+      self.$dot.style.opacity = 1;
+      self.$outline.style.opacity = 1;
     } else {
-      self.$dot.style.opacity = 0
-      self.$outline.style.opacity = 0
+      self.$dot.style.opacity = 0;
+      self.$outline.style.opacity = 0;
     }
   }
-}
+};
 
-cursor.init()
+cursor.init();
 
 window.transitionToPage = function(href, color) {
   if (color) {
-    document.body.style.backgroundColor = color
+    document.body.style.backgroundColor = color;
+  } else {
+    document.body.style.backgroundColor = "var(--ro)";
   }
-  document.body.style.opacity = 0
+  document.body.style.opacity = 0;
   setTimeout(function() {
-    window.location.href = href
-  }, 1000)
-}
+    window.location.href = href;
+  }, 500);
+};
 
 function AddClass() {
-  document.body.className += 'fade-out'
+  document.body.className += "fade-out";
   setTimeout(() => {
-    RemoveClass()
-  }, 500)
+    RemoveClass();
+  }, 500);
 }
 
-AddClass()
+AddClass();
 
 function RemoveClass() {
-  document.body.classList.remove('fade-out')
+  document.body.classList.remove("fade-out");
 }
--- js/index2.0.js
+++ js/index2.0.js
@@ -1,144 +1,174 @@
 "use strict";
-var pnls = document.querySelectorAll('.section').length,
-    scdir, hold = false;
+var pnls = document.querySelectorAll(".section").length,
+  scdir,
+  hold = false;
 var current = 0;
-var main = document.getElementById('main');
+var main = document.getElementById("main");
 var sections = main.childElementCount - 1;
 
 function _scrollY(obj, slength) {
-    var plength, pan,
-        vh = window.innerHeight / 100,
-        vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
-    if (hold === true) return;
-    if ((this !== undefined && this.id === 'main') || (obj !== undefined && obj.id === 'main')) {
-        pan = this || obj;
-        plength = parseInt(pan.offsetHeight / vh);
-    }
-    if (pan === undefined) {
-        return;
-    }
-    plength = plength || parseInt(pan.offsetHeight / vmin);
-    if (scdir === 'down' && current != -100 * sections) {
-        current -= slength;
-        slength *= -1;
-    } else if (scdir === 'up' && current < 0) {
-        current += slength;
-    } else if (scdir === 'top') {
-        current = 0;
-        slength = 0;
-    }
-    switch (current) {
-        case 0:
-            main.style.backgroundColor = 'rgb(12, 12, 12)';
-            break;
-        case -100:
-            main.style.backgroundColor = 'rgb(11, 52, 110)';
-            break;
-        case -200:
-            main.style.backgroundColor = 'rgb(237, 120, 74)';
-            break;
-        case -300:
-            main.style.backgroundColor = 'rgb(203, 27, 69)';
-            break;
-        case -400:
-            main.style.backgroundColor = 'rgb(252, 250, 242)';
-            break;
-        default:
-            main.style.backgroundColor = 'rgb(12, 12, 12)';
-            break;
-    }
-    if (hold === false) {
-        hold = true;
-        setTimeout(function () { hold = false; }, 1000);
-        pan.style.transform = 'translateY(' + current + 'vh)';
-    }
-    console.log('current: ' + current + ' ' + scdir + ':' + slength + ':' + plength + ':' + (plength - plength / pnls));
+  var plength,
+    pan,
+    vh = window.innerHeight / 100,
+    vmin = Math.min(window.innerHeight, window.innerWidth) / 100;
+  if (hold === true) return;
+  if (
+    (this !== undefined && this.id === "main") ||
+    (obj !== undefined && obj.id === "main")
+  ) {
+    pan = this || obj;
+    plength = parseInt(pan.offsetHeight / vh);
+  }
+  if (pan === undefined) {
+    return;
+  }
+  plength = plength || parseInt(pan.offsetHeight / vmin);
+  if (scdir === "down" && current != -100 * sections) {
+    current -= slength;
+    slength *= -1;
+  } else if (scdir === "up" && current < 0) {
+    current += slength;
+  } else if (scdir === "top") {
+    current = 0;
+    slength = 0;
+  }
+  switch (current) {
+    case 0:
+      main.style.backgroundColor = "rgb(12, 12, 12)";
+      break;
+    case -100:
+      main.style.backgroundColor = "rgb(11, 52, 110)";
+      break;
+    case -200:
+      main.style.backgroundColor = "rgb(237, 120, 74)";
+      break;
+    case -300:
+      main.style.backgroundColor = "rgb(203, 27, 69)";
+      break;
+    case -400:
+      main.style.backgroundColor = "rgb(252, 250, 242)";
+      break;
+    default:
+      main.style.backgroundColor = "rgb(12, 12, 12)";
+      break;
+  }
+  document.body.style.backgroundColor = main.style.backgroundColor;
+  if (hold === false) {
+    hold = true;
+    setTimeout(function() {
+      hold = false;
+    }, 1000);
+    pan.style.transform = "translateY(" + current + "vh)";
+  }
+  console.log(
+    "current: " +
+      current +
+      " " +
+      scdir +
+      ":" +
+      slength +
+      ":" +
+      plength +
+      ":" +
+      (plength - plength / pnls)
+  );
 }
 
 function _swipe(obj) {
-    var swdir,
-        sX,
-        sY,
-        dX,
-        dY,
-        threshold = 100,
-        /*[min distance traveled to be considered swipe]*/
-        slack = 50,
-        /*[max distance allowed at the same time in perpendicular direction]*/
-        alT = 500,
-        /*[max time allowed to travel that distance]*/
-        elT, /*[elapsed time]*/
-        stT; /*[start time]*/
-    obj.addEventListener('touchstart', function (e) {
-        var tchs = e.changedTouches[0];
-        swdir = 'none';
-        sX = tchs.pageX;
-        sY = tchs.pageY;
-        stT = new Date().getTime();
-        //e.preventDefault();
-    }, false);
+  var swdir,
+    sX,
+    sY,
+    dX,
+    dY,
+    threshold = 100,
+    /*[min distance traveled to be considered swipe]*/
+    slack = 50,
+    /*[max distance allowed at the same time in perpendicular direction]*/
+    alT = 500,
+    /*[max time allowed to travel that distance]*/
+    elT /*[elapsed time]*/,
+    stT; /*[start time]*/
+  obj.addEventListener(
+    "touchstart",
+    function(e) {
+      var tchs = e.changedTouches[0];
+      swdir = "none";
+      sX = tchs.pageX;
+      sY = tchs.pageY;
+      stT = new Date().getTime();
+      //e.preventDefault();
+    },
+    false
+  );
 
-    obj.addEventListener('touchmove', function (e) {
-        e.preventDefault(); /*[prevent scrolling when inside DIV]*/
-    }, false);
+  obj.addEventListener(
+    "touchmove",
+    function(e) {
+      e.preventDefault(); /*[prevent scrolling when inside DIV]*/
+    },
+    false
+  );
 
-    obj.addEventListener('touchend', function (e) {
-        var tchs = e.changedTouches[0];
-        dX = tchs.pageX - sX;
-        dY = tchs.pageY - sY;
-        elT = new Date().getTime() - stT;
-        if (elT <= alT) {
-            if (Math.abs(dX) >= threshold && Math.abs(dY) <= slack) {
-                swdir = (dX < 0) ? 'left' : 'right';
-            } else if (Math.abs(dY) >= threshold && Math.abs(dX) <= slack) {
-                swdir = (dY < 0) ? 'down' : 'up';
-            }
-            if (obj.id === 'main') {
-                if (swdir === 'down') {
-                    scdir = swdir;
-                    _scrollY(obj);
-                } else if (swdir === 'up' && obj.style.transform !== 'translateY(0)') {
-                    scdir = swdir;
-                    _scrollY(obj);
-
-                }
-                e.stopPropagation();
-            }
+  obj.addEventListener(
+    "touchend",
+    function(e) {
+      var tchs = e.changedTouches[0];
+      dX = tchs.pageX - sX;
+      dY = tchs.pageY - sY;
+      elT = new Date().getTime() - stT;
+      if (elT <= alT) {
+        if (Math.abs(dX) >= threshold && Math.abs(dY) <= slack) {
+          swdir = dX < 0 ? "left" : "right";
+        } else if (Math.abs(dY) >= threshold && Math.abs(dX) <= slack) {
+          swdir = dY < 0 ? "down" : "up";
         }
-    }, false);
+        if (obj.id === "main") {
+          if (swdir === "down") {
+            scdir = swdir;
+            _scrollY(obj);
+          } else if (
+            swdir === "up" &&
+            obj.style.transform !== "translateY(0)"
+          ) {
+            scdir = swdir;
+            _scrollY(obj);
+          }
+          e.stopPropagation();
+        }
+      }
+    },
+    false
+  );
 }
 
 function registerChildren() {
-    for (let i = 1; i < sections; i++) {
-        main.children[0].children[0].children[i].addEventListener('click', function () {
-            scdir = 'down';
-            _scrollY(main, 100 * (i));
-        });
-    }
+  for (let i = 1; i < sections; i++) {
+    main.children[0].children[0].children[i].addEventListener(
+      "click",
+      function() {
+        scdir = "down";
+        _scrollY(main, 100 * i);
+      }
+    );
+  }
 }
 registerChildren();
 
-main.style.transform = 'translateY(0)';
-main.addEventListener('wheel', function (e) {
-    if (e.deltaY < 0) {
-        scdir = 'up';
-    }
-    else
-        scdir = 'down';
-    _scrollY(main, 100);
-    e.stopPropagation();
+main.style.transform = "translateY(0)";
+main.addEventListener("wheel", function(e) {
+  if (e.deltaY < 0) {
+    scdir = "up";
+  } else scdir = "down";
+  _scrollY(main, 100);
+  e.stopPropagation();
 });
-main.addEventListener('wheel', _scrollY);
+main.addEventListener("wheel", _scrollY);
 _swipe(main);
-var tops = document.querySelectorAll('.top');
+var tops = document.querySelectorAll(".top");
 for (let i = 0; i < tops.length; i++) {
-    tops[i].addEventListener('click', function () {
-        console.log(tops[i].parentElement);
-        scdir = 'top';
-        _scrollY(main);
-    });
+  tops[i].addEventListener("click", function() {
+    console.log(tops[i].parentElement);
+    scdir = "top";
+    _scrollY(main);
+  });
 }
-
-document.addEventListener('DOMContentLoaded', function (event) {
-    document.querySelector('body').style.opacity = 1
-})
