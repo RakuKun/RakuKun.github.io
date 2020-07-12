5e0c9e20d50e7493dadf1f90b78eeaf1d31e793e
Liangzheng Luo <liangzheng0730@gmail.com>
2020-07-11 23:52:15 -0400

    7.11 Major Update
    More content for ditto and level design pages. Re-constructed nav page for better reusability. Some general fixes.

---
+++ js/nav.js
@@ -0,0 +1,136 @@
+function CommentDoc(fn) {
+  return fn.toString().split('\n').slice(1, -1).join('\n') + '\n';
+}
+
+var tmpl = CommentDoc(function () {
+  /*
+    <span id="cursor-outline"></span>
+    <span id="cursor"></span>
+    <div id='home'>
+      <a onclick="transitionToPage('../')"> ◯ </a>
+    </div>
+    <div id='menu'>
+      <a> ≡ </a>
+    </div>
+    <div id='menu-page'>
+      <div class='category'>
+        <div class='entry-title'>Games</div>
+        <a
+          class='entry'
+          onclick="transitionToPage('../games/bvw','var(--rurikon)')">
+          Building Virtual Worlds
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../games/ditto','var(--rurikon)')">
+          Project Ditto
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../games/level-design','var(--rurikon)')">
+          Level Design
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../games/cosmos','var(--rurikon)')">
+          Cosmos
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../games/collection','var(--rurikon)')">
+          Collection
+        </a>
+      </div>
+      <div class='category'>
+        <div class='entry-title'>Analysis</div>
+        <a
+          class='entry'
+          onclick="transitionToPage('../analysis/bloodborne','var(--sohi)')">
+          Bloodborne: Story in Levels
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../analysis/slay-the-spire','var(--sohi)')">
+          Slay the Spire: Engagement and Balancing
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../analysis/inside','var(--sohi)')">
+          INSIDE: Narrative and Visuals
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../analysis/the-plan','var(--sohi)')">
+          The Plan: An Experiment
+        </a>
+      </div>
+      <div class='category'>
+        <div class='entry-title'>Gallery</div>
+        <a
+          class='entry'
+          onclick="transitionToPage('../gallery/art','var(--kurenai)')">
+          Art
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../gallery/photography','var(--kurenai)')">
+          Photography
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../gallery/design','var(--kurenai)')">
+          Design
+        </a>
+      </div>
+      <div class='category'>
+        <div class='entry-title'>Blog</div>
+        <a
+          class='entry'
+          onclick="transitionToPage('../blog/quest-mmorpg','var(--shironeri)')">
+          Creating Quests for MMORPG
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../blog/formulas','var(--shironeri)')">
+          Formulas in Combat Design
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../blog/balance','var(--shironeri)')">
+          Balancing a Game
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../blog/design-diary','var(--shironeri)')">
+          Design Diary
+        </a>
+        <br />
+        <a
+          class='entry'
+          onclick="transitionToPage('../blog/another-inferno','var(--shironeri)')">
+          Another Inferno
+        </a>
+      </div>
+    </div>
+*/
+});
+
+var newHtml = tmpl.substring(5, tmpl.length - 5);
+
+console.log(newHtml);
+
+var nav = document.getElementById('nav');
+
+nav.innerHTML = newHtml;
